3f87cc9371fdb0ef29c0cdc7ff8c45fe7dd5b9d9
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-11-25 21:36:27 -0300

    Criacao do script.js

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+let nome = document.querySelector('#nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+
+
+function validarNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validarEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1) {
+        txtEmail.innerHTML = 'E-mail Inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true ) {
+        alert('Enviado com sucesso!')
+    } else {
+        alert('Preencha as informações corretamente antes de enviar.')
+    }
+}
